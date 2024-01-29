8f5cb33073799b8b230146292b6597c0113c88a1
nickoo20 <gniyati20@gmail.com>
2024-01-30 00:25:29 +0530

    First Template

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+   <>
+    <h1 className='text-4xl bg-slate-700 text-white'>MernBlog Application</h1>
+   </>
+  )
+}
+
+export default App
